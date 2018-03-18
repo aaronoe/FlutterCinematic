99a9bbd6badf125af937022f3e60a6d4ee7e5691
aaron <aaronoe97@gmail.com>
2018-03-18 13:51:02 +0100

    type: feat - add genre utility methods

--- lib/util/utils.dart
+++ lib/util/utils.dart
@@ -4,6 +4,34 @@ import 'package:url_launcher/url_launcher.dart';
 final dollarFormat = new NumberFormat("#,##0.00", "en_US");
 final sourceFormat = new DateFormat('yyyy-MM-dd');
 final dateFormat = new DateFormat.yMMMMd("en_US");
+Map<int, String> genreMap = {
+  28: 'Action',
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western'
+};
+
+List<String> getGenresForIds(List<int> genreIds) => genreIds.map((id) => genreMap[id]).toList();
+
+String getGenreString(List<int> genreIds) =>
+    getGenresForIds(genreIds)
+        .fold(new StringBuffer(), (buffer, String value) => buffer.write(value))
+        .toString();
 
 String formatNumberToDollars(int amount) => '\$${dollarFormat.format(amount)}';
 
